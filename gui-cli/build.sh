ee585adcad6d8923ce1313ed348ea095b18240b3
itzNOELdev <dev.zodium.project@gmail.com>
2026-07-02 16:28:19 +0530

    Fix : remove eza , its in fedora repos now

--- gui-cli/build.sh
+++ gui-cli/build.sh
@@ -47,7 +47,7 @@ dnf makecache --refresh
 ok "Terra repo added"
 
 info "Downloading packages from Terra..."
-dnf download eza eza-zsh-completion \
+dnf download \
     scx-scheds scx-tools brave-origin brave-keyring code starship \
     ghostty ghostty-nautilus ghostty-zsh-completion ghostty-terminfo ghostty-shell-integration ghostty-bat-syntax ghostty-neovim ghostty-kio \
     --destdir /output \
